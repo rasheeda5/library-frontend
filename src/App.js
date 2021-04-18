9a2c414a05b0b070b87ae8706ea6e2cc45501f13
Rasheeda Abbas <rasheedaabbas@gmail.com>
2021-04-18 23:26:40 +0500

    routes + get all books

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import { useState, useEffect, useMemo } from "react";
+// import { useState, useEffect, useMemo } from "react";
 import Table from "./Table";
 import "./App.css";
 
 function App() {
-  const columns = useMemo(() => [
-    {
-      Header: "Books",
-      columns: [
-        {
-          Header: "Name",
-          accessor: book.title,
-        },
-        {
-          Header: "Year of Publishing",
-          accessor: book.publishYear,
-        },
-        {
-          Header: "Cover Price",
-          accessor: book.coverPrice,
-        },
-        {
-          Header: "ISBN",
-          accessor: book.ISBN,
-        },
-        {
-          Header: "Status",
-          accessor: book.status,
-        },
-      ],
-    },
-  ]);
-
-  getTableData = () => {};
-  return useEffect(() => {});
+  return <Table />;
 }
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,3 +1,52 @@
-function Table() {}
+import { useEffect } from "react";
+import axios from "axios";
+
+const Table = () => {
+  useEffect(() => {
+    const url = "http://localhost:8080/books";
+    axios.get(url).then((response) => console.log(response.data));
+  }, []);
+
+  //   const res = axios.get(url);
+  //   console.log(res);
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+};
+
+// function Table() {
+//     state = {
+//         loading: true,
+//         person: null
+//       };
+
+//       componentDidMount = async function () {
+//         const url = "http://localhost:8080/books";
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         this.setState({ books: data.results[0], loading: false });
+//       }
+
+//       render() {
+//         if (this.state.loading) {
+//           return <div>loading...</div>;
+//         }
+
+//         // if (!this.state.person) {
+//         //   return <div>didn't get data</div>;
+//         // }
+
+//         return (
+//             console.log(data.results)
+//         //   <div>
+//         //     <div>{this.state.person.name.title}</div>
+//         //     <div>{this.state.person.name.first}</div>
+//         //     <div>{this.state.person.name.last}</div>
+//         //     <img src={this.state.person.picture.large} />
+//         //   </div>
+//         );
+//       }
 
 export default Table;
